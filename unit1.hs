d969575fd136ae0bc3ba58eaf54d5cdc0d756e31
Alejandro Alcalde <algui91@gmail.com>
2019-03-26 16:32:19 +0100

    sumSquareorSquareSum

--- unit1.hs
+++ unit1.hs
@@ -1,1 +1,7 @@
 
+-- SumSquareOrSquareSum using lambda
+
+sumSquareorSquareSum x y = (\sumSquare squareSum ->
+                              if sumSquare > squareSum
+                              then sumSquare
+                              else squareSum) (x^2 + y^2) ((x+y)^2)
