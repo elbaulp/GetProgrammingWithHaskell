adb6d41d85a3f8f17d583d88f99bf60a1e9040e4
Alejandro Alcalde <alejandro.alcalde@moia.io>
2021-02-07 07:51:57 +0100

    Chapter 4 exercises

--- HaskellProgramming/ch4.hs
+++ HaskellProgramming/ch4.hs
@@ -12,4 +12,27 @@ changeMood :: Mood -> Mood
 changeMood Woot = Blah
 changeMood _    = Woot
 
--- Go on and Bool me
+-- Chapter exercises
+awesome :: [String]
+awesome = ["Papuchon", "curry", ":)"]
+alsoAwesome :: [String]
+alsoAwesome = ["Quake", "The Simons"]
+allAwesome :: [[String]]
+allAwesome = [awesome, alsoAwesome]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == reverse x
+
+myAbs :: Integer -> Integer
+myAbs x = if x<0 then -x else x
+
+f :: (a, b) -> (c, d) -> ((b,d), (a,c))
+f x y = ((snd x, snd y), (fst x, fst y))
+
+main :: IO ()
+main = do
+  print $ length (concat allAwesome)
+  print $ isPalindrome ["a","b","a" ]
+  print $ isPalindrome "ABA"
+  print $ myAbs 10
+  print $ myAbs (-10)
